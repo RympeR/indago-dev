857be3cf512cd580bd0f287ffd86e7d36c67cb14
Andrey Vynogradov <40463796+WeRtOG@users.noreply.github.com>
2023-04-07 14:55:41 +0200

    Adds php backend

---
+++ backend/send.php
@@ -0,0 +1,67 @@
+<?php
+
+error_reporting(E_ERROR | E_PARSE);
+header('Content-Type: application/json; charset=utf-8');
+
+// Telegram API token
+$token = '6152127034:AAFgmqi2hYrVDVrMxiFoZqXr0ZCPNqs6RyY';
+$chatId = '-1001749067057';
+
+
+// POST data
+$fullName = $_GET['name'] ?? null;
+$subject = $_GET['subject'] ?? null;
+$email = $_GET['email'] ?? null;
+$message = $_GET['message'] ?? null;
+
+// Prepared message for Telegram
+$preparedMessage = urlencode(
+    "🔔 *Получено новое сообщение*\n\n" .
+        "*Полное имя*: $fullName\n" .
+        "*Тема сообщения*: $subject\n" .
+        "*E-mail*: $email\n" .
+        "*Сообщение*: \n\n" .
+        $message
+);
+
+// If everything is alright
+if ($fullName != null && $subject != null && $email != null && $message != null) {
+    try {
+        $response = file_get_contents("https://api.telegram.org/bot$token/sendMessage?text=$preparedMessage&chat_id=$chatId&parse_mode=markdown");
+
+        if ($response) {
+            http_response_code(200);
+
+            echo json_encode([
+                'ok' => true,
+                'code' => 200,
+            ], JSON_PRETTY_PRINT);
+        } else {
+            http_response_code(500);
+
+            echo json_encode([
+                'ok' => false,
+                'code' => 500,
+                'message' => 'Something went wrong...'
+            ], JSON_PRETTY_PRINT);
+        }
+        // If some shit happens...
+    } catch (Exception $error) {
+        http_response_code(500);
+
+        echo json_encode([
+            'ok' => false,
+            'code' => 500,
+            'message' => $error->__toString()
+        ], JSON_PRETTY_PRINT);
+    }
+    // If user forgot to enter some data
+} else {
+    http_response_code(400);
+
+    echo json_encode([
+        'ok' => false,
+        'code' => 400,
+        'message' => 'Bad request.'
+    ], JSON_PRETTY_PRINT);
+}
